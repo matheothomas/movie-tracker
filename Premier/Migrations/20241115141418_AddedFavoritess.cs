352e83485eab6074916ffbed17a20adf0aff8d87
Elloim <eloi.petit.pro@protonmail.com>
2024-11-15 15:17:50 +0100

    Favorites work

---
+++ Premier/Migrations/20241115141418_AddedFavoritess.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Premier.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedFavoritess : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Favorite",
+                table: "Favorite");
+
+            migrationBuilder.RenameTable(
+                name: "Favorite",
+                newName: "Favorites");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Favorites",
+                table: "Favorites",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Favorites",
+                table: "Favorites");
+
+            migrationBuilder.RenameTable(
+                name: "Favorites",
+                newName: "Favorite");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Favorite",
+                table: "Favorite",
+                column: "Id");
+        }
+    }
+}
