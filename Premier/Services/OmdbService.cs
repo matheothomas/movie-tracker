f0bb369dcf8b5c67b386e60b40e128a789dd8895
matheothomas <103116889+matheothomas@users.noreply.github.com>
2024-11-22 15:52:34 +0100

    Added OMDB GET query

--- Premier/Services/OmdbService.cs
+++ Premier/Services/OmdbService.cs
@@ -1,8 +1,27 @@
-namespace Premier.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Premier.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Core;
 
-public class OmdbService {
-    public List<Film> SearchByTitle(String title) {
+namespace Premier.Service {
+
+  public class OmdbService {
+    HttpClient _client;
+    private readonly string _apiKey;
+
+    public OmdbService(HttpClient client, IConfiguration configuration) {
+      _client = client;
+      _apiKey = configuration["APIKey"];
     }
-    public Film GetByImdbId(String imdbId) {
+
+    public async List<Film> SearchByTitle(string title) {
+      HttpResponseMessage response = await client.GetAsync($"http://www.omdbapi.com/?i={title}&apikey={_apiKey}");
+      // CALL JSON convert
     }
+
+    public Film GetByImdbId(string imdbId) {
+    }
+  }
 }
