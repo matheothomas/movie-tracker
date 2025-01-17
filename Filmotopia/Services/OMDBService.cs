d8de0e13735353d38c2b00c2fec91e5d18b761dd
Domi <domitillevale@gmail.com>
2025-01-17 15:08:25 +0100

    add a film in the database using search

--- Filmotopia/Services/OMDBService.cs
+++ Filmotopia/Services/OMDBService.cs
@@ -13,4 +13,11 @@ public class OMDBService {
         var film = await _httpClient.GetFromJsonAsync<List<Film>>($"http://localhost:5041/api/OMDB/search?title={s}");
         return film;
     }
+
+    public async Task<Film> ImportFilm(string filmId)
+    {
+        var film = await _httpClient.GetFromJsonAsync<Film>($"http://localhost:5041/api/OMDB/import?imdbID={filmId}");
+        return film;
+    }
+
 }
